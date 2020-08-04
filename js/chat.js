040ed4e0a36bb7d7378593bb4c8f7f6dd85ccd26
Jun Zheng <56985601+JunZheng-dev@users.noreply.github.com>
2020-08-04 17:36:18 -0400

    added elements + chat changes when tf changes

--- js/chat.js
+++ js/chat.js
@@ -1,14 +1,35 @@
-const client = new tmi.Client({
+var client = new tmi.Client({
 	connection: {
 		secure: true,
 		reconnect: true
 	},
-	channels: ["mizkif"]
+	channels: [""]
 });
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
-	document.getElementById("message").innerHTML = 
-		tags['display-name'] + ": " + message;
+	document.getElementById("user-name").innerHTML = 
+		tags['display-name'] + ": ";
+	document.getElementById("user-message").innerHTML = message;
+});
+
+jQuery("#channel-textfield").on("input propertychange paste", function() {
+	client.disconnect();
+
+    client = new tmi.Client({
+		connection: {
+			secure: true,
+			reconnect: true
+		},
+		channels: [document.getElementById("channel-textfield").value]
+	});
+
+	client.connect();
+
+	client.on('message', (channel, tags, message, self) => {
+		document.getElementById("user-name").innerHTML = 
+			tags['display-name'] + ": ";
+		document.getElementById("user-message").innerHTML = message;
+	});
 });
