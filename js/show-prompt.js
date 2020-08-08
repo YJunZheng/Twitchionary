ce2ad3dfa1ddebe2d72a4a46e7a9edaba1fa966f
Jun Zheng <56985601+JunZheng-dev@users.noreply.github.com>
2020-08-08 00:07:18 -0400

    fixed first new prompt

--- js/show-prompt.js
+++ js/show-prompt.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
-	if (localStorage.promptVisibility == "true") {
-		document.getElementById("prompt-container").innerHTML = localStorage.prompt;
-		document.getElementById("show-prompt-button").innerHTML = "Hide Prompt";
-	} else {
-		document.getElementById("prompt-container").innerHTML = "? ? ?";
-		document.getElementById("show-prompt-button").innerHTML = "Show Prompt";
-	}
-})
+if (localStorage.promptVisibility == "true") {
+	document.getElementById("prompt-container").innerHTML = localStorage.prompt;
+	document.getElementById("show-prompt-button").innerHTML = "Hide Prompt";
+} else {
+	document.getElementById("prompt-container").innerHTML = "? ? ?";
+	document.getElementById("show-prompt-button").innerHTML = "Show Prompt";
+}
+
 
 function showPrompt() { //occurrs when prompt visibility button is pressed
 	if (localStorage.promptVisibility == "true") {
